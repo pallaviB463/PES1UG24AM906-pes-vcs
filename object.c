403c36c79ef51504dfa85dc093d90410b2d5d65d
Pallavi B <pallavibasavarajappa01@gmail.com>
2026-04-21 12:12:37 +0000

    Phase 1: added file writing logic

--- object.c
+++ object.c
@@ -9,7 +9,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <openssl/evp.h>
-
 // ─── PROVIDED ────────────────────────────────────────────────────────────────>
 void hash_to_hex(const ObjectID *id, char *hex_out) {
     for (int i = 0; i < HASH_SIZE; i++) {
